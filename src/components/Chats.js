1053a3b13ffa6e98225c33e2b37bd9e5212bf497
umeree <umernadeam@gmail.com>
2023-08-11 16:20:59 +0500

    Api's Completed

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
 import { useAuth } from "../contexts/AuthContext";
+import { axios } from "axios";
 
 const Chats = () => {
   const history = useHistory();
   const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
 
   console.log(user);
 
@@ -16,6 +18,51 @@ const Chats = () => {
     await auth.signOut();
     history.push("/");
   };
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+
+      return;
+    }
+    axios
+      .get("http://api.chatengine.io/user/me", {
+        headers: {
+          "project-id": "00ecc894-295a-4828-ba12-7666acc627f6",
+          "user-name": user.email,
+          "user-secret": user.eid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formdata = new FormData();
+        formdata.append("email", user.email);
+        formdata.append("username", user.email);
+        formdata.append("secret", user.uid);
+        getFile(user.photoURL).then((avatar) => {
+          formdata.append("avatar", avatar, avatar.name);
+
+          axios
+            .post("http://api.chatengine.io/users", formdata, {
+              headers: {
+                "private-key": "b17522f0-96a5-4f6e-b94d-1b6c1a60d4ec",
+              },
+            })
+            .then(() => setLoading(false))
+            .catch((error) => console.log(error));
+        });
+      });
+  }, [user, history]);
+
+  if (!user || loading) return "Loading...";
   return (
     <div className="chats-page">
       <div className="nav-bar">
@@ -26,9 +73,9 @@ const Chats = () => {
       </div>
       <ChatEngine
         height="calc(100vh - 66px)"
-        projectId="00ecc894-295a-4828-ba12-7666acc627f6"
-        userName="."
-        userSecret="."
+        projectID="00ecc894-295a-4828-ba12-7666acc627f6"
+        userName={user.email}
+        userSecret={user.uid}
       />
     </div>
   );
